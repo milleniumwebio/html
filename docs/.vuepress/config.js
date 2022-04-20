af90c1773ececa199301220a28f126f943fae139
NYU Tandon Online <info@millenniumweb.io>
2022-04-20 14:34:50 -0400

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,61 +5,6 @@ module.exports = {
         smoothScroll: true,
         lastUpdated: 'Last Updated',
         nav: [
-            { text: 'Updating',
-            items: [
-                {text: 'Cinema 4D', items:[
-                    {text: 'C4D', link: '/Cinema 4D/'},
-                    {text: 'Octane', link: '/Cinema 4D/Octane/'},
-                ]},
-                {text: 'After Effects', items:[
-                    {text: 'AE', link: '/AE/'},
-                    {text: 'Pending', link: '/Cinema 4D/Octane/'},
-                ]},
-            ]  
-        },
-            { text: 'Updating',
-            items: [
-                {text: 'Front-End', items:[
-                    {text: 'p5.js', link: '/Code/Front-End/p5js/'},
-                    {text: 'Vue', link: '/Code/Front-End/vuejs/'},
-                ]},
-                {text: 'Swift UI', items:[
-                    {text: 'Pending', link: '/AE/'},
-                    {text: 'Pending', link: '/Cinema 4D/Octane/'},
-                ]},
-            ]  
-        },
-        { text: '语法',
-            items: [
-                {text: '时态', link: '/Learn/Grammar/Tense/'},
-                {text: 'Pending', link: '/Cinema 4D/Octane/'},
-            ]  
-        },
-        { text: 'PT',
-            items: [
-                {text: 'Personal Training', link: '/Exercise/'},
-                {text: 'CPT', link: '/Exercise/CPT/'},
-                {text: 'Exercise Science', link: '/Exercise/ExerciseScience/'},
-                {text: 'Human Anatomy', link: '/Exercise/Anatomy/'},
-            ]  
-        },
-        { text:'Learn',
-            items: [
-                {text: '2021 Fall', items:[
-                    {text: '3D', link: '/nyu/3D/'},
-                    {text: 'Thesis', link: '/nyu/thesis/'},
-                ]},
-                {text: '2021 Spring', items:[
-                    {text: 'Pre-Thesis', link: '/nyu/prethesis/'},
-                ]},
-                {text: '2020 Fall', items:[
-                    {text: 'Creative Coding', link: '/nyu/creativecoding/'},
-                    {text: 'VR', link:'/nyu/vr/'},
-                    {text: 'UX', link:'/nyu/ux/'},
-                    {text: 'I&P', link:'/nyu/IandP/'}
-                ]},
-            ] 
-        },
             { text: 'Our Main Website', link: 'https://millenniumweb.io/'},
         ],
         sidebar: 'auto'
--- docs/README.md
+++ docs/README.md
@@ -3,16 +3,16 @@ home: true
 heroImage: images/hero.png
 heroText: Millenium Web Strategies
 tagline: Still updating
-actionText: 回到Portfolio
-actionLink: https://google.com
+actionText: Back to MWS
+actionLink: https://millenniumweb.io/
 features:
-- title: Motion
-  details: 也就随便做做动画
-- title: Exercise Science
-  details: 选择上方的Fitness项目
-- title: 其他功能还在更新
-  details: 更多请讲
-footer: MIT Licensed Powered by VuePress | Copyright © 2022 | Irwin Qi
+- title: Features A
+  details: Features A
+- title: Features B
+  details: Features B
+- title: Features C
+  details: Features C
+footer: MIT Licensed Powered by VuePress | Copyright © 2022 MWS - All rights reserved
 ---
 
 # Welcome 
